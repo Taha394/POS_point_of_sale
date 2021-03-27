ddb1be466da73ffaccfd864ce58c14e270621bd0
Taha394 <Salahtaha741@gmail.com>
2021-03-27 21:48:01 +0200

    mypos

---
+++ resources/lang/en/site.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'logout' => 'Logout',
+
+    'add' => 'add',
+    'create' => 'create',
+    'read' => 'read',
+    'edit' => 'edit',
+    'update' => 'update',
+    'delete' => 'delete',
+    'search' => 'search',
+    'show' => 'show',
+    'loading' => 'loading',
+    'print' => 'Print',
+    'confirm_delete' => 'confirm delete',
+    'yes' => 'yes',
+    'no' => 'no',
+
+    'login' => 'login',
+    'remember_me' => 'remember_me',
+    'password' => 'password',
+    'password_confirmation' => 'password confirmation',
+
+    'added_successfully' => 'added successfully',
+    'updated_successfully' => 'updated successfully',
+    'deleted_successfully' => 'deleted successfully',
+
+    'no_data_found' => 'no data found',
+    'no_records' => 'no records',
+
+    'clients' => 'Clients',
+    'client_name' => 'client name',
+    'phone' => 'phone',
+    'address' => 'address',
+    'previous_orders' => 'previous orders',
+    'orders' => 'Orders',
+    'add_order' => 'add order',
+    'edit_order' => 'edit order',
+
+    'users' => 'Moderators',
+    'first_name' => 'first name',
+    'last_name' => 'last name',
+    'email' => 'Email',
+    'image' => 'Image',
+    'action' => 'Control',
+
+    'permissions' => 'permissions',
+
+    'categories' => 'Categories',
+    'all_categories' => 'all categories',
+    'name' => 'name',
+    'description' => 'Description',
+    'products_count' => 'Products count',
+    'related_products' => 'Related products',
+    'category' => 'Category',
+    'show_products' => 'Show products',
+    'created_at' => 'Created at',
+
+    'products' => 'Products',
+    'product' => 'Product',
+    'quantity' => 'Quantity',
+    'total' => 'Total',
+    'purchase_price' => 'Purchase price',
+    'price' => 'Price',
+    'sale_price' => 'Sale price',
+    'stock' => 'Stock',
+    'profit_percent' => 'Profit percent',
+
+    'ar' => [
+        'name' => 'الاسم باللغه العربيه',
+        'description' => 'الوصف باللغه العربيه',
+    ],
+
+    'en' => [
+        'name' => 'الاسم باللغه الانجليزيه',
+        'description' => 'الوصف باللغه الانجليزيه',
+    ],
+
+];
